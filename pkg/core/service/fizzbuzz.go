fd5d216c0ece90c6150ca0f0de31659af7d547ee
jzsaiyan <njhanzeeb31@gmail.com>
2021-10-08 16:07:51 +0200

    optimize perf

--- pkg/core/service/fizzbuzz.go
+++ pkg/core/service/fizzbuzz.go
@@ -14,10 +14,9 @@ func NewFizzBuzz() *FizzBuzz {
 
 // Process fizzbuzz.
 func (f *FizzBuzz) Process(int1, int2, limit int, str1, str2 string) []string {
-	var (
-		s   string
-		arr []string
-	)
+	var s string
+
+	arr := make([]string, 0, limit)
 
 	for i := 1; i <= limit; i++ {
 		if i%int1 == 0 && i%int2 == 0 {
